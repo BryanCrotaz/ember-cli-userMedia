a309b432f18b92907e3e9bc4243f88822e584532
Bryan Crotaz <bgcrotaz@hotmail.com>
2018-08-05 01:15:20 +0100

    use ember-concurrency

--- addon/components/user-media-src.js
+++ addon/components/user-media-src.js
@@ -1,9 +1,8 @@
 import Component from '@ember/component';
 import { set, get, observer } from '@ember/object';
-//import { on } from '@ember/object/evented';
 import RSVP from 'rsvp';
 import layout from '../templates/components/user-media-src';
-//import polyfill from '../helpers/webRTC-polyfill';
+import { task } from 'ember-concurrency';
 
 const {resolve} = RSVP;
 
@@ -27,45 +26,46 @@ export default Component.extend({
 		this._revokeSrc = URL ? URL.revokeObjectURL : function(stream) {return stream;};
 	},
 
-	_startStream() {
+	_startStream: task(function * () {
 		if (!navigator || !navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
 			set(this, 'error', "getUserMedia not available");
 			return resolve();
 		}
 		set(this, 'error', null);
 		let constraints = get(this, 'mediaConstraints');
-		return navigator.mediaDevices.getUserMedia(constraints)
-		.then(stream => {
+		try {
+			let stream = yield navigator.mediaDevices.getUserMedia(constraints)
 			set(this, 'videoUrl', this._createSrc(stream));
-		})
-		.catch(err => {
+		} catch (err) {
 			// eslint-disable-next-line
 			console.error("error creating user media stream: "+err+"\nconstraints: "+JSON.stringify(constraints, null, 2));
 			set(this, 'videoUrl', null);
 			set(this, 'error', err);
-		});
-	},
-
-	_stopStream() {		
-		this._revokeSrc(get(this, 'videoUrl'));
-		if (!this.get('isDestroying') && !this.get('isDestroyed')) {
-			set(this, 'videoUrl', null);
 		}
-	},
+	}),
+
+	_stopStream: task(function * () {
+		yield this._revokeSrc(get(this, 'videoUrl'));
+		set(this, 'videoUrl', null);
+	}),
+
+	_restartStream: task(function * () {
+		yield this.get('stopStream').perform();
+		yield this.get('startStream').perform();
+	}).restartable(),
 
 	_updateStream: observer("mediaConstraints", "mediaConstraints.video", "mediaConstraints.audio", function() {
-		this._stopStream();
-		this._startStream();
+		this.get('_restartStream').perform()
 	}),
 
 	didInsertElement () {
 		this._super(...arguments);
-		this._startStream();
+		this.get('_startStream').perform();
 	},
 
 	willDestroyElement () {
 		this._super(...arguments);
-		this._stopStream();
+		this.get('_stopStream').perform();
 	}
 
 });
