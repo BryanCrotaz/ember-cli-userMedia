bb8ebd5929ad6666d7024ff8b581210129e5de9f
Bryan Crotaz <bgcrotaz@hotmail.com>
2019-08-03 12:50:07 +0100

    run codemods

--- tests/integration/components/user-media-src-test.js
+++ tests/integration/components/user-media-src-test.js
@@ -1,25 +1,27 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('user-media-src', 'Integration | Component | user media src', {
-  integration: true
-});
+module('Integration | Component | user media src', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+  test('it renders', async function(assert) {
+    
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
 
-  this.render(hbs`{{user-media-src}}`);
+    await render(hbs`{{user-media-src}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.dom('*').hasText('');
 
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#user-media-src}}
-      template block text
-    {{/user-media-src}}
-  `);
+    // Template block usage:" + EOL +
+    await render(hbs`
+      {{#user-media-src}}
+        template block text
+      {{/user-media-src}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.dom('*').hasText('template block text');
+  });
 });
